da07efafc9525775da8edaad23a9bd720fb6bda1
Daniel Maricic <daniel@woss.io>
2022-05-20 12:15:00 +0000

    [skip ci] update the utils/url
    Signed-off-by: Daniel Maricic <daniel@woss.io>

--- apps/app/src/utils/url.ts
+++ apps/app/src/utils/url.ts
@@ -4,13 +4,15 @@ import { alerts } from '$src/components/notifications/stores';
 /**
  * Parse the hash string and return the the value for the given name
  * @param fullHash - The value of `$page.url.hash`
- * @param hashName - Name of the hash which comes in `nam=value` format
- * @returns decoded value via `decodeURIComponent`
+ * @param hashName - Name of the hash which comes in `name=value` format
+ * @param defaultValue - If passed it will be used if not defaults to '' and will fail
+ * @returns decoded value via `decodeURIComponent` or default value
  */
 export function getHashValue(fullHash: string, hashName: string, defaultValue = ''): string {
   console.log('getHashValue', { hashName, fullHash });
   if (isEmpty(fullHash)) return '';
 
+  // remove the `#` from the string
   const parts = split('&')(fullHash.substring(1));
 
   const foundValue = filter((p) => startsWith(hashName, p), parts);
