dd32fa0cdd4c049de4d38db9a722779ce9e64376
Daniel Maricic <daniel@woss.io>
2022-10-03 13:15:35 +0000

    Resolve "remove anagolay.dev app"

--- common/scripts/add-package-json-to-libs.sh
+++ common/scripts/add-package-json-to-libs.sh
@@ -5,10 +5,10 @@ PKG_MODULE=$GITPOD_REPO_ROOT/common/scripts/package.json/module.json
 PKG_CJS=$GITPOD_REPO_ROOT/common/scripts/package.json/commonjs.json
 
 cp "$PKG_MODULE" "$GITPOD_REPO_ROOT/sdk/api/lib/package.json"
-cp "$PKG_CJS" "$GITPOD_REPO_ROOT/sdk/api/lib_cjs/package.json"
+cp "$PKG_CJS" "$GITPOD_REPO_ROOT/sdk/api/lib-cjs/package.json"
 
 cp "$PKG_MODULE" "$GITPOD_REPO_ROOT/sdk/types/lib/package.json"
-cp "$PKG_CJS" "$GITPOD_REPO_ROOT/sdk/types/lib_cjs/package.json"
+cp "$PKG_CJS" "$GITPOD_REPO_ROOT/sdk/types/lib-cjs/package.json"
 
 cp "$PKG_MODULE" "$GITPOD_REPO_ROOT/sdk/util/lib/package.json"
-cp "$PKG_CJS" "$GITPOD_REPO_ROOT/sdk/util/lib_cjs/package.json"
+cp "$PKG_CJS" "$GITPOD_REPO_ROOT/sdk/util/lib-cjs/package.json"
